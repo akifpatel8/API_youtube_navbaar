1f14f6677792178d330986db23efa3ad5ad75311
akifpatel <aakifpatel08@gmail.com>
2021-07-30 13:38:00 -0700

    changed API

--- day2/we.js
+++ day2/we.js
@@ -4,16 +4,16 @@ let videos_div=document.getElementById("videos")
 async function defaultVideos(){
     videos_div.innerHTML=null
     
-    let res=await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=most_popular_videos_in_india&key=AIzaSyCqqEL64PvDYgjKhg7PkiDl-4MM2W4sLL0&maxResults=21`);
+    let res=await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=AIzaSyCqqEL64PvDYgjKhg7PkiDl-4MM2W4sLL0&maxResults=21`);
     let data=await res.json()
     console.log(data);
    
     let {items}=data;
-    items = items.filter((el) => {return el.id.videoId != undefined})
-    items.forEach(({id:{ videoId } } ) => {
+    items = items.filter((el) => {return el.id != undefined})
+    items.forEach(({id} ) => {
         let div=document.createElement("div")
          div.setAttribute("class","each-video")
-        div.innerHTML=`<iframe width="260" height="180" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        div.innerHTML=`<iframe width="260" height="180" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
         videos_div.appendChild(div)
 
     })
@@ -29,7 +29,7 @@ async function findVideos(){
     let q=document.getElementById("query").value;
     let res=await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyCqqEL64PvDYgjKhg7PkiDl-4MM2W4sLL0&maxResults=21`);
     let data=await res.json()
-    console.log(data);
+    // console.log(data);
    
     let {items}=data;
     items = items.filter((el) => {return el.id.videoId != undefined})
